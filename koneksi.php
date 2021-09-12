1507b7d1420c25d9b406c3893c8ddc6b14a06829
azhari <azharisaputrasikumbang@gmail.com>
2021-09-13 01:38:45 +0700

    improve runtime configuration

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,17 @@
 <?php
-$koneksi = mysqli_connect('localhost','root','','kapekoding');
+
+// set server environment first
+define(ENVIRONMENT, 'stage');
+
+if (ENVIRONMENT !== 'prod') {
+	ini_set('display_errors', 1);
+	ini_set('error_reporting', 1);
+}
+
+$koneksi = mysqli_connect('host','username','password','databasename');
+
+if (!$koneksi) {
+	die("Database error : " . mysqli_connect_error());	
+}
+
 ?>
