568a55bc77477e53a2584d70d14c23f51bc4c421
azhari <azharisaputrasikumbang@gmail.com>
2021-09-14 22:19:44 +0700

    rewrite to prod first

--- koneksi.php
+++ koneksi.php
@@ -3,9 +3,9 @@
 // set server environment first
 define(ENVIRONMENT, 'stage');
 
-if (ENVIRONMENT !== 'prod') {
-	ini_set('display_errors', 1);
-	ini_set('error_reporting', 1);
+if (ENVIRONMENT == 'prod') {
+	ini_set('display_errors', 0);
+	ini_set('error_reporting', 0);
 }
 
 $koneksi = mysqli_connect('host','username','password','databasename');
